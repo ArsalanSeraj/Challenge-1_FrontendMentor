43c59fce9d88f469b18e64fe467374ce1cd558c4
Arsalan <amirseraj.ir@gmail.com>
2022-08-07 14:42:55 +0430

    Challenge finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "desktop-pattern": "url('./images/bg-desktop.svg')",
+        "mobile-pattern": "url('./images/bg-mobile.svg')",
+      },
+      colors: {
+        VioletBg: "hsl(257, 40%, 49%)",
+        SoftMagenta: "hsl(300, 69%, 71%)",
+      },
+      fontFamily: {
+        "ff-headings": ["Poppins", "sans-serif"],
+        "ff-text": ["OpenSans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
